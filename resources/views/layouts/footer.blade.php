61cc7140b4d1d5a73cb9902d5ea4041ff5f308c4
Farhan <f.asif2010@gmail.com>
2020-11-17 15:18:14 +0600

    auth

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,8 +15,6 @@
 <script src="./dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="./dist/js/demo.js"></script>
-
 @yield('customjs')
-
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('pages.home.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
